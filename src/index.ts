0be4898712b00dae3d31976d3a6a83123339aaf0
kngval <bkvalteocs@gmail.com>
2023-12-26 15:16:33 +0800

    arrays & intellisense

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,17 @@ let sample; // any type
 function anyType(params: any){
     console.log(params);
 }
+
+//TS Arrays
+//again, specifying data types is optional in this case, ts already does it for you
+let numbers :number[] = [1,2,3,4,5] //Only numbers are allowed, otherwise it will throw an error
+
+
+//but there are also cases like this
+let number = [] // the data type here is any, and we should avoid that by specifying the data type in this case a number
+number[0] = 1;
+// number[1] = '1'  //this will throw an error
+
+numbers.forEach(i => i.toLocaleString) // TS intellisense depends on the data type
+
+
